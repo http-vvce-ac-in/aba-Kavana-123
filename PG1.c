a19f12a7fcc05c1b512832e20c7f14bdf479040a
Kavana-123 <112940799+Kavana-123@users.noreply.github.com>
2022-09-06 16:56:20 +0530

    Update PG1.c

--- PG1.c
+++ PG1.c
@@ -1,3 +1,26 @@
 //Program to check vowel or not 
 //Input :a
 //Output:a is a vowel
+#include<stdio.h>
+void main()
+{
+  char alph;
+  printf("Enter the alphabet:");
+  scanf("%c",&alph);
+  switch(alph)
+  {
+      case'a':printf("Vowel");
+              break;
+      case'e':printf("Vowel");
+              break;
+      case'i':printf("Vowel");
+              break;
+      case'o':printf("Vowel");
+              break;
+      case'u':printf("Vowel");
+              break;
+    default: printf("consonant");
+             break;
+  }
+}
+      
